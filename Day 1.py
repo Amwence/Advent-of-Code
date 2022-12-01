604ef409025f179dd7967c8a8c5a09f82c6bb9b4
Ding Dong Doodly Duder <Amwence@users.noreply.github.com>
2022-12-01 14:13:21 -0600

    Sure

---
+++ Day 1.py
@@ -0,0 +1,27 @@
+data = open('input.txt', 'r')
+
+for line in input:
+    input.readline()
+    int(line)
+    if line == []:
+        #Change index of elf
+        
+    elif elf == Elf5 and line == []:
+        for elf in elves:
+            for num in inventory:
+                inventory += num
+    else:
+        append.list(elf@index)
+Elf1 = []
+Elf2 = []
+Elf3 = []
+Elf4 = []
+Elf5 = []
+elves = [Elf1,Elf2,Elf3,Elf4,Elf5]
+#seperate input into elves inventory seperated by line.
+# add all elves to index and append list for elves until blank line occurs then move to next index repeat till elf 5 has full index then run comparison 
+
+
+#stop after 5 elves.
+
+#add numbers together for total inventory and compare totals for elves and compare to find highest number
